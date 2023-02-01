02c452f26991bc34cd9f140dda7ff7373c35aadb
Ezequiel <zapata.ed1989@gmail.com>
2023-02-01 16:37:35 -0300

    token acces

--- src/api/gitHubApi.ts
+++ src/api/gitHubApi.ts
@@ -3,7 +3,9 @@ import axios from 'axios';
 
 export const gitHubApi = axios.create({
     baseURL: 'https://api.github.com/repos/facebook/react',
-    headers: {
-        Authorization: 'Bearer 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'
-    }
+
+    // Agregar headers de autorizacion para sumar peticiones en github, con un token acces valido 
+    // headers: {
+    //     Authorization: 'Bearer github_pat_{{  TOKEN ACCES }}'
+    // }
 })
